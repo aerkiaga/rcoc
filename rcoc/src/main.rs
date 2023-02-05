13d7a378b8eb48f269e4f39c69d21b01b777f767
Aritz Erkiaga <aerkiaga3@gmail.com>
2023-02-05 11:46:33 +0100

    Make it CLI-friendly

--- rcoc/src/main.rs
+++ rcoc/src/main.rs
@@ -2,18 +2,26 @@ mod ast;
 mod kernel;
 mod parser;
 
-const code: &str = "
-/*comment*/
-//comment
-let a:
-    @(T: Prop, x: T) T
-=
-    |T: Prop, x: T| {|y: T| y}(x)
-;
-";
+struct ProgramConfiguration {
+    file_to_open: Option<String>,
+}
+
+fn parse_args(args: &Vec<String>) -> ProgramConfiguration {
+    match args.len() {
+        1 => ProgramConfiguration {file_to_open: None,},
+        _ => ProgramConfiguration {file_to_open: Some(args[1].clone()),},
+    }
+}
 
 fn main() {
-    let parser_result = parser::parse(code);
+    let args = std::env::args().collect::<Vec<String>>();
+    let program_configuration = parse_args(&args);
+    let file_to_open = match program_configuration.file_to_open {
+        None => panic!("No input files"),
+        Some(f) => f,
+    };
+    let code = std::fs::read_to_string(file_to_open).expect("Could not read file");
+    let parser_result = parser::parse(&code);
     let ast = match parser_result {
         Ok(x) => x,
         Err(e) => panic!("{:?}", e),
