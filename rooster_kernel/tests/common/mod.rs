3cb353a7d031fe4a1d45e3e7fa64c6499036c378
Aritz Erkiaga <aerkiaga3@gmail.com>
2023-03-01 02:29:21 +0100

    Attempt to add inductive type tests

--- rooster_kernel/tests/common/mod.rs
+++ rooster_kernel/tests/common/mod.rs
@@ -15,40 +15,32 @@ fn parse_identifier(input: &[char]) -> (String, &[char]) {
 }
 
 fn parse_application(input: &[char], previous_term: Term) -> (Term, &[char]) {
-    if input.len() >= 1 && input[0] == ' ' {
-        let (mut next_term, rest_of_input) = parse_term(&input[1..]);
-        let mut current_term = &mut next_term;
-        loop {
-            if let Term::Application {
-                function_term: inner_function_term,
-                parameter_term: _,
-                debug_context: _,
-            } = current_term
-            {
-                current_term = &mut *inner_function_term;
-            } else {
-                break;
-            }
+    let mut current_term = previous_term.clone();
+    let mut current_input = input;
+    loop {
+        if current_input.len() >= 1 && current_input[0] == ' ' {
+            let (next_term, rest_of_input) = parse_term(&current_input[1..], false);
+            current_input = rest_of_input;
+            current_term = Term::Application {
+                function_term: Box::new(current_term),
+                parameter_term: Box::new(next_term),
+                debug_context: TermDebugContext::Ignore,
+            };
+        } else {
+            break;
         }
-        *current_term = Term::Application {
-            function_term: Box::new(previous_term),
-            parameter_term: Box::new(current_term.clone()),
-            debug_context: TermDebugContext::Ignore,
-        };
-        (next_term, rest_of_input)
-    } else {
-        (previous_term, input)
     }
+    (current_term, current_input)
 }
 
-fn parse_term(input: &[char]) -> (Term, &[char]) {
+fn parse_term(input: &[char], try_application: bool) -> (Term, &[char]) {
     match input[0] {
         'λ' | '∀' | '𝐘' => {
             let (binding_identifier, input2) = parse_identifier(&input[1..]);
             assert!(input2[0] == ':');
-            let (binding_type, input3) = parse_term(&input2[1..]);
+            let (binding_type, input3) = parse_term(&input2[1..], true);
             assert!(input3[0] == '.');
-            let (value_term, rest_of_input) = parse_term(&input3[1..]);
+            let (value_term, rest_of_input) = parse_term(&input3[1..], true);
             (
                 match input[0] {
                     'λ' => Term::Lambda {
@@ -75,16 +67,32 @@ fn parse_term(input: &[char]) -> (Term, &[char]) {
             )
         }
         '(' => {
-            let (inner_term, input2) = parse_term(&input[1..]);
+            let (inner_term, input2) = parse_term(&input[1..], true);
             assert!(input2[0] == ')');
             let rest_of_input = &input2[1..];
-            parse_application(rest_of_input, inner_term)
+            if try_application {
+                parse_application(rest_of_input, inner_term)
+            } else {
+                (inner_term, rest_of_input)
+            }
+        }
+        '?' => {
+            let identifier_term = Term::Identifier("?".to_string(), TermDebugContext::Ignore);
+            if try_application {
+                parse_application(&input[1..], identifier_term)
+            } else {
+                (identifier_term, &input[1..])
+            }
         }
         ch => {
             assert!(ch == '_' || ch.is_alphanumeric());
             let (identifier_name, rest_of_input) = parse_identifier(input);
             let identifier_term = Term::Identifier(identifier_name, TermDebugContext::Ignore);
-            parse_application(rest_of_input, identifier_term)
+            if try_application {
+                parse_application(rest_of_input, identifier_term)
+            } else {
+                (identifier_term, rest_of_input)
+            }
         }
     }
 }
@@ -94,8 +102,8 @@ pub fn execute(code: &[(&str, &str, &str)]) {
     for statement in code {
         match state.try_define(
             &statement.0.to_string(),
-            parse_term(&statement.1.chars().collect::<Vec<_>>()).0,
-            parse_term(&statement.2.chars().collect::<Vec<_>>()).0,
+            parse_term(&statement.1.chars().collect::<Vec<_>>(), true).0,
+            parse_term(&statement.2.chars().collect::<Vec<_>>(), true).0,
         ) {
             Ok(_) => (),
             Err(_) => panic!(),
---
+++ rooster_kernel/tests/inductive_tests.rs
@@ -0,0 +1,43 @@
+mod common;
+
+#[test]
+fn define_nat() {
+    common::execute(&[("nat", "Set", "𝐘self:Set.∀T:? Set.∀_:T.∀_:∀_:self.T.T")]);
+}
+
+#[test]
+fn define_nat_constructors() {
+    common::execute(&[
+        ("nat", "Set", "𝐘self:Set.∀T:? Set.∀_:T.∀_:∀_:self.T.T"),
+        ("O", "nat", "λT:? nat.λa:T.λb:∀_:nat.T.a"),
+        ("S", "∀_:nat.nat", "λx:nat.λT:? nat.λa:T.λb:∀_:nat.T.b x"),
+    ]);
+}
+
+#[test]
+fn define_add() {
+    common::execute(&[
+        ("nat", "Set", "𝐘self:Set.∀T:? Set.∀_:T.∀_:∀_:self.T.T"),
+        ("O", "nat", "λT:? nat.λa:T.λb:∀_:nat.T.a"),
+        ("S", "∀_:nat.nat", "λx:nat.λT:? nat.λa:T.λb:∀_:nat.T.b x"),
+        (
+            "add",
+            "∀_:nat.∀_:nat.nat",
+            "𝐘self:(∀_:nat.∀_:nat.nat).λn:nat.λm:nat.n (nat) m (λp:(nat).S (self p m))",
+        ),
+    ]);
+}
+
+#[test]
+fn inductive_axiom() {
+    common::execute(&[
+        ("nat", "Set", "𝐘self:Set.∀T:? Set.∀_:T.∀_:∀_:self.T.T"),
+        ("O", "nat", "λT:? nat.λa:T.λb:∀_:nat.T.a"),
+        ("S", "∀_:nat.nat", "λx:nat.λT:? nat.λa:T.λb:∀_:nat.T.b x"),
+        (
+            "nat_induction",
+            "∀P:∀_:nat.Prop.∀_:P O.∀_:∀n:nat.∀_:P n.P (S n).∀n:nat.P n",
+            "𝐘self:∀P:∀_:nat.Prop.∀_:P O.∀_:∀n:nat.∀_:P n.P (S n).∀n:nat.P n.λP:∀_:nat.Prop.λpO:P O.λh:∀n:nat.∀_:P n.P (S n).λn:nat.n (P n) pO (λp:nat.h p (self P pO h p))",
+        ),
+    ]);
+}
